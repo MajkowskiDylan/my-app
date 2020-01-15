d24aed896bdc56c65d81f40e48696968cbe82d99
MajkowskiDylan <majkowskidylan@gmail.com>
2020-01-15 11:56:11 +0100

    Interface Login

---
+++ src/app/Tile.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+
+/**
+ * @title Dynamic grid-list
+ */
+@Component({
+  selector: 'grid-list-dynamic-example',
+  templateUrl: 'grid-list-dynamic-example.html',
+  styleUrls: ['grid-list-dynamic-example.css'],
+})
+export class GridListDynamicExample {
+
+}
